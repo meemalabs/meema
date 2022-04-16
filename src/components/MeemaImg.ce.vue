66dec983e9e81c149081001c02062971c200dee4
Chris <chrisbreuer93@gmail.com>
2022-04-15 22:28:59 -0700

    chore: wip

--- src/components/MeemaImg.ce.vue
+++ src/components/MeemaImg.ce.vue
@@ -1,55 +1,33 @@
 <template>
-  <div class="w-full h-12 bg-black" style="height: 60px">
-    <div ref="meema" class="w-1/2 h-12 bg-red-500" style="height: 60px">
+  <div class="h-12 w-full bg-black" style="height: 60px">
+    <div ref="meema" class="h-12 w-1/2 bg-red-500" style="height: 60px">
       <!-- <img :src="url" /> -->
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed, onMounted } from 'vue';
 
-export default defineComponent({
-  name: 'MeemaImg',
+interface Props {
+  src: string;
+  width?: number | null;
+  w?: number | null;
+  height?: number | null;
+  h?: number | null;
+  // blurhash, lazy, alt,
+}
 
-  props: {
-    src: {
-      type: String,
-      required: true
-      // TODO: add validation rule
-    },
-
-    // TODO: blurhash, lazy, alt, etc.
-
-    width: {
-      type: Number,
-      default: null
-    },
-
-    w: {
-      type: Number,
-      default: null
-    },
-
-    height: {
-      type: Number,
-      default: null
-    },
-
-    h: {
-      type: Number,
-      default: null
-    }
-  },
-
-  computed: {
-    url() {
-      return `https://dev-cdn.mee.ma/${this.src}?w=${this.width}`;
-    }
-  },
-
-  mounted() {
-    console.log('this.$refs.meema.clientHeight', this.$refs.meema);
-  }
+withDefaults(defineProps<Props>(), {
+  width: null,
+  w: null,
+  height: null,
+  h: null
 });
+
+onMounted(() => {
+  // console.log('$refs', this.$refs.meema);
+});
+
+const url = computed(() => `https://dev-cdn.mee.ma/${src}?w=${width}`);
 </script>
