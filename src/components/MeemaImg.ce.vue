4329741a3973d7ef395f2e9b19927d06693f8645
Chris <chrisbreuer93@gmail.com>
2022-04-16 13:32:33 -0700

    chore: wip

--- src/components/MeemaImg.ce.vue
+++ src/components/MeemaImg.ce.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="h-12 w-full bg-black" style="height: 60px">
-    <div ref="meema" class="h-12 w-1/2 bg-red-500" style="height: 60px">
-      <!-- <img :src="url" /> -->
-    </div>
+  <div ref="meema" class="h-12 w-1/2 bg-red-500" style="height: 60px">
+    <img :src="url" />
   </div>
 </template>
 
@@ -21,10 +19,13 @@ interface Props {
 const props = defineProps<Props>();
 
 const url = computed(
-  () => `https://dev-cdn.mee.ma/${props.src}?w=${props.width}`
+  // () => `https://dev-cdn.mee.ma/${props.src}?w=${props.width}`
+  () =>
+    `https://images.pexels.com/photos/11039600/pexels-photo-11039600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
 );
 
 onMounted(() => {
   console.log('url', url);
+  console.log('props', props);
 });
 </script>
